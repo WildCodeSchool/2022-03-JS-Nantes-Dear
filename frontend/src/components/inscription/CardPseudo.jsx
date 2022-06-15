e7609f368e77cd160f644d9ed7ab4ca73e89567f
Aline Saint-Lanne <aline.saint-lanne@orange.fr>
2022-06-15 16:40:30 +0200

    page inscription + composant pseudo

---
+++ frontend/src/components/inscription/CardPseudo.jsx
@@ -0,0 +1,52 @@
+import { React, useState } from "react";
+import "./styles/CardPseudo.css";
+import ButtonContinue from "./ButtonContinue";
+
+export default function CardPseudo() {
+  const [pseudo, setPseudo] = useState("");
+
+  function handleSend() {}
+
+  return (
+    <div className="bloc-card-pseudo">
+      <div className="title-card">
+        <h1>Procédons ensemble à ton inscription</h1>
+        <h2>PSEUDO ET AVATAR</h2>
+        <h3>Et oui, ici tout est anonyme</h3>
+      </div>
+      <div className="bloc-pseudo">
+        <form className="form-pseudo">
+          <label className="pseudoform" htmlFor="pseudo">
+            <h2 className="stringform">Pseudo</h2>
+            <input
+              className="inputformpseudo"
+              id="pseudo"
+              type="text"
+              placeholder="Indique ton pseudo"
+              value={pseudo}
+              onChange={(e) => setPseudo(e.target.value)}
+            />
+          </label>
+          <h2 className="stringform">Ma tranche d'âge</h2>
+          <select className="option-age" id="share-select">
+            <option value="">--Choisir une tranche d'âge--</option>
+            <option value="">18 - 25 ans</option>
+            <option value="">26 - 35 ans</option>
+            <option value="">36 - 45 ans</option>
+            <option value="">45 et +</option>
+          </select>
+          <button
+            className="inputformsubmit"
+            type="button"
+            onClick={handleSend}
+          >
+            Valider
+          </button>
+        </form>
+      </div>
+      <div className="button-continue">
+        <ButtonContinue />
+      </div>
+    </div>
+  );
+}
