5557055405149313524fe17142e8bdaee30edbc5
SeverineVil <severinevilleneuve@gmail.com>
2022-06-16 17:03:54 +0200

    modif pr register

--- frontend/src/pages/Body.jsx
+++ frontend/src/pages/Body.jsx
@@ -2,14 +2,20 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Inscription from "./Inscription";
 import Intro from "./Intro";
+import IntroAdult from "./IntroAdult";
+import SorryNotAdult from "../components/intro/SorryNotAdult";
+
 
 export default function Body() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Intro />} />
-        <Route path="Home" element={<Home />} />
-        <Route path="Inscription" element={<Inscription />} />
+        <Route path="/introAdult" element={<IntroAdult />} />
+        <Route path="/sorryNotAdult" element={<SorryNotAdult />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/inscription" element={<Inscription />} />
+        <Route path="/test" element={<Register />} /> 
       </Routes>
     </div>
   );
