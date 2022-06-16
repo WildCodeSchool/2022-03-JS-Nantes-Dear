74dfb59661d842df144924617c639aa2c0dba2f9
Lucie Chauvet <lucie.chauvet10@gmail.com>
2022-06-16 12:18:43 +0200

    CSS component SorryNotAdult

--- frontend/src/components/intro/SorryNotAdult.jsx
+++ frontend/src/components/intro/SorryNotAdult.jsx
@@ -9,15 +9,19 @@ function SorryNotAdult() {
         <img className="logo-not-adult" src={logo} alt="logo Dear blanc" />
       </div>
       <div className="text-sorry">
-        <h2 className="sorry-title">Désolé...</h2>
-        <p>
-          Nous sommes navrés, l'appli DEAR s'adresse uniquement à des personnes
-          MAJEURES.
-        </p>
-        <p className="insta-pssst">
-          Pssst, par contre tu peux nous suivre sur INSTA : dear_officiel_
-        </p>
-        <p className="take-care">Take care.</p>
+        <div className="first-bloc">
+          <h2 className="sorry-title">Désolé...</h2>
+          <p className="text-adult">
+            Nous sommes navrés, l'appli DEAR s'adresse uniquement à des
+            personnes MAJEURES.
+          </p>
+        </div>
+        <div className="second-bloc">
+          <p className="insta-pssst">
+            Pssst, par contre tu peux nous suivre sur INSTA : dear_officiel_
+          </p>
+          <p className="take-care">Take care.</p>
+        </div>
       </div>
     </div>
   );
