2d1890cbceedbfccf736b55e8ffb9fda31bc6a6b
Lucie Chauvet <lucie.chauvet10@gmail.com>
2022-06-23 17:16:10 +0200

    settings arrows buttons + starting desktop version

--- frontend/src/components/profil/UserAccount.jsx
+++ frontend/src/components/profil/UserAccount.jsx
@@ -8,6 +8,8 @@ import { FiFileText } from "react-icons/fi";
 import "./styles/UserAccount.css";
 
 function UserAccount() {
+  const [category, setCategory] = React.useState("");
+
   return (
     <div className="user-account-page">
       <div className="profile-top">
@@ -17,29 +19,42 @@ function UserAccount() {
       <div className="profile-card">
         <div className="profile-category">
           <BsListTask className="profile-icon" />
-          <h3>Mes données personnelles</h3>
-          <IoIosArrowDroprightCircle className="arrow-profile" />
+          <h3 value={category}>Mes données personnelles</h3>
+          <button type="button">
+            <IoIosArrowDroprightCircle className="arrow-profile" />
+          </button>
         </div>
         <div className="profile-category">
           <AiFillCustomerService className="profile-icon" />
           <h3>Contacter Dear</h3>
-          <IoIosArrowDroprightCircle className="arrow-profile" />
+          <button type="button">
+            <IoIosArrowDroprightCircle className="arrow-profile" />
+          </button>
         </div>
         <div className="profile-category">
           <VscWorkspaceTrusted className="profile-icon" />
           <h3>Charte de Confiance</h3>
-          <IoIosArrowDroprightCircle className="arrow-profile" />
+          <button type="button">
+            <IoIosArrowDroprightCircle className="arrow-profile" />
+          </button>
         </div>
         <div className="profile-category">
           <HiOutlineChatAlt2 className="profile-icon" />
           <h3>F.A.CUL</h3>
-          <IoIosArrowDroprightCircle className="arrow-profile" />
+          <button type="button">
+            <IoIosArrowDroprightCircle className="arrow-profile" />
+          </button>
         </div>
         <div className="profile-category">
           <FiFileText className="profile-icon" />
           <h3>Mentions légales</h3>
-          <IoIosArrowDroprightCircle className="arrow-profile" />
+          <button type="button">
+            <IoIosArrowDroprightCircle className="arrow-profile" />
+          </button>
         </div>
+      </div>
+      <div>
+        <h1>{setCategory}</h1>
       </div>
       <div className="profile-logout">
         <h4>Me déconnecter</h4>
