d1e76fe81a328aa100835ba011d464d31bb2bba9
Lucie Chauvet <lucie.chauvet10@gmail.com>
2022-06-28 14:13:03 +0200

    display categories

--- frontend/src/components/profil/UserAccount.jsx
+++ frontend/src/components/profil/UserAccount.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoIosArrowDroprightCircle } from "react-icons/io";
 import { BsListTask } from "react-icons/bs";
 import { AiFillCustomerService } from "react-icons/ai";
@@ -6,10 +6,13 @@ import { VscWorkspaceTrusted } from "react-icons/vsc";
 import { HiOutlineChatAlt2 } from "react-icons/hi";
 import { FiFileText } from "react-icons/fi";
 import "./styles/UserAccount.css";
-import UserAccountCategory from "./UserAccountCategory";
 
 function UserAccount() {
-  const [category, setCategory] = React.useState("");
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
 
   return (
     <div className="user-account-page">
@@ -17,52 +20,137 @@ function UserAccount() {
         <h1>Pseudo</h1>
         <button type="button">Modifier mon profil</button>
       </div>
-      <div className="profile-card">
-        <div className="profile-category">
-          <BsListTask className="profile-icon" />
-          <h3 value={category}>Mes données personnelles</h3>
-          <button type="button">
-            <IoIosArrowDroprightCircle className="arrow-profile" />
-          </button>
+      <div className="container-profile">
+        <div className="profile-card">
+          <div className="profile-category">
+            <BsListTask className="profile-icon" />
+            <h3>Mes données personnelles</h3>
+            <button
+              type="button"
+              className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+              onClick={() => toggleTab(1)}
+            >
+              <IoIosArrowDroprightCircle className="arrow-profile" />
+            </button>
+          </div>
+          <div className="profile-category">
+            <AiFillCustomerService className="profile-icon" />
+            <h3>Contacter Dear</h3>
+            <button
+              type="button"
+              className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+              onClick={() => toggleTab(2)}
+            >
+              <IoIosArrowDroprightCircle className="arrow-profile" />
+            </button>
+          </div>
+          <div className="profile-category">
+            <VscWorkspaceTrusted className="profile-icon" />
+            <h3>Charte de Confiance</h3>
+            <button
+              type="button"
+              className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+              onClick={() => toggleTab(3)}
+            >
+              <IoIosArrowDroprightCircle className="arrow-profile" />
+            </button>
+          </div>
+          <div className="profile-category">
+            <HiOutlineChatAlt2 className="profile-icon" />
+            <h3>F.A.CUL</h3>
+            <button
+              type="button"
+              className={toggleState === 4 ? "tabs active-tabs" : "tabs"}
+              onClick={() => toggleTab(4)}
+            >
+              <IoIosArrowDroprightCircle className="arrow-profile" />
+            </button>
+          </div>
+          <div className="profile-category">
+            <FiFileText className="profile-icon" />
+            <h3>Mentions légales</h3>
+            <button
+              type="button"
+              className={toggleState === 5 ? "tabs active-tabs" : "tabs"}
+              onClick={() => toggleTab(5)}
+            >
+              <IoIosArrowDroprightCircle className="arrow-profile" />
+            </button>
+          </div>
+          <div className="profile-logout">
+            <h4>Me déconnecter</h4>
+            <p>Supprimer mon compte</p>
+          </div>
         </div>
-        <div className="profile-category">
-          <AiFillCustomerService className="profile-icon" />
-          <h3>Contacter Dear</h3>
-          <button type="button">
-            <IoIosArrowDroprightCircle className="arrow-profile" />
-          </button>
+        <div className="content-tabs">
+          <div
+            className={
+              toggleState === 1 ? "content  active-content" : "content"
+            }
+          >
+            <h2>Mes données personnelles</h2>
+            <hr />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+              praesentium incidunt quia aspernatur quasi quidem facilis quo
+              nihil vel voluptatum?
+            </p>
+          </div>
+          <div
+            className={
+              toggleState === 2 ? "content  active-content" : "content"
+            }
+          >
+            <h2>Contacter Dear</h2>
+            <hr />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+              voluptatum qui adipisci.
+            </p>
+          </div>
+          <div
+            className={
+              toggleState === 3 ? "content  active-content" : "content"
+            }
+          >
+            <h2>Charte de Confiance</h2>
+            <hr />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed
+              nostrum rerum laudantium totam unde adipisci incidunt modi alias!
+              Accusamus in quia odit aspernatur provident et ad vel distinctio
+              recusandae totam quidem repudiandae omnis veritatis nostrum
+              laboriosam architecto optio rem, dignissimos voluptatum beatae
+              aperiam voluptatem atque. Beatae rerum dolores sunt.
+            </p>
+          </div>
+          <div
+            className={
+              toggleState === 4 ? "content  active-content" : "content"
+            }
+          >
+            <h2>F.A.CUL</h2>
+            <hr />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+              praesentium incidunt quia aspernatur quasi quidem facilis quo
+              nihil vel voluptatum?
+            </p>
+          </div>
+          <div
+            className={
+              toggleState === 5 ? "content  active-content" : "content"
+            }
+          >
+            <h2>Mentions légales</h2>
+            <hr />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+              praesentium incidunt quia aspernatur quasi quidem facilis quo
+              nihil vel voluptatum?
+            </p>
+          </div>
         </div>
-        <div className="profile-category">
-          <VscWorkspaceTrusted className="profile-icon" />
-          <h3>Charte de Confiance</h3>
-          <button type="button">
-            <IoIosArrowDroprightCircle className="arrow-profile" />
-          </button>
-        </div>
-        <div className="profile-category">
-          <HiOutlineChatAlt2 className="profile-icon" />
-          <h3>F.A.CUL</h3>
-          <button type="button">
-            <IoIosArrowDroprightCircle className="arrow-profile" />
-          </button>
-        </div>
-        <div className="profile-category">
-          <FiFileText className="profile-icon" />
-          <h3>Mentions légales</h3>
-          <button type="button">
-            <IoIosArrowDroprightCircle className="arrow-profile" />
-          </button>
-        </div>
-      </div>
-      <div>
-        <h1>{setCategory}</h1>
-      </div>
-      <div className="profile-logout">
-        <h4>Me déconnecter</h4>
-        <p>Supprimer mon compte</p>
-      </div>
-      <div className="bloc-profile-category">
-        <UserAccountCategory />
       </div>
     </div>
   );
