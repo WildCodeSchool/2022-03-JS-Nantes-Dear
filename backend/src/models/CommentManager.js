43796b02c9e444b4287a84af2da8c16cebf70867
Lucie Chauvet <lucie.chauvet10@gmail.com>
2022-07-04 10:27:31 +0200

    user profile page

---
+++ backend/src/models/CommentManager.js
@@ -0,0 +1,25 @@
+const AbstractManager = require("./AbstractManager");
+
+class CommentManager extends AbstractManager {
+  static table = "Comment";
+
+  insert(Comment) {
+    return this.connection.query(
+      `insert into ${CommentManager.table} (created_at, user_id, post_id,) values (?, ?, ?)`,
+      [Comment.created_at, Comment.user_id, Comment.post_id]
+    );
+  }
+
+  findAll() {
+    return this.connection.query(`SELECT `);
+  }
+
+  update(Comment) {
+    return this.connection.query(
+      `update ${CommentManager.table} set name = ? where id = ?`,
+      [Comment.created_at, Comment.user_id, Comment.post_id]
+    );
+  }
+}
+
+module.exports = CommentManager;
