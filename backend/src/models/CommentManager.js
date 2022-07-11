cbc37c44824566d90875506c63455b9c82d3b977
Nathan Conty <83788253+Natecty@users.noreply.github.com>
2022-07-11 11:47:14 +0200

    ok

--- backend/src/models/CommentManager.js
+++ backend/src/models/CommentManager.js
@@ -7,7 +7,6 @@ class CommentManager extends AbstractManager {
     return this.connection.query(
       `select * from ${comment.table} where comment = ?`,
       [comment]
-
     );
   }
 
@@ -28,7 +27,6 @@ class CommentManager extends AbstractManager {
     return this.connection.query(
       `update ${CommentManager.table} set user_id = ?, post_id = ?, created_at = ? where id = ?`,
       [comment.user_id, comment.post_id, comment.created_at]
-
     );
   }
 }
