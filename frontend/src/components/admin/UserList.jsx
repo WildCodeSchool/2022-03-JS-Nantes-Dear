6d95ace98a249f47258252ece096066e784851a8
Aline Saint-Lanne <aline.saint-lanne@orange.fr>
2022-07-13 17:15:24 +0200

    modif page UserList

--- frontend/src/components/admin/UserList.jsx
+++ frontend/src/components/admin/UserList.jsx
@@ -4,13 +4,11 @@ import "./styles/UserList.css";
 import iconcross from "../../assets/icon-cross.png";
 import iconeye from "../../assets/icon-eye.png";
 
-// eslint-disable-next-line react/prop-types
-function UserList({ pseudo }) {
+function UserList() {
   return (
     <div className="container-user-list">
       <div className="align-pseudo-pictos">
         <div className="user-list">
-          <p className="user-pseudo">{pseudo}</p>
           <p className="user-pseudo-exemple">Petitfenouil10</p>
         </div>
         <div className="pictos-list-user">
