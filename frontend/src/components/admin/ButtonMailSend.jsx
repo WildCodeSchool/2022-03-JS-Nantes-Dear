d094699173a32d53617258c8b1900138ac67fbc9
Aline Saint-Lanne <aline.saint-lanne@orange.fr>
2022-07-15 22:53:57 +0200

    PAGE ADMIN BACK : mail suppression profil utilisateur

--- frontend/src/components/admin/ButtonMailSend.jsx
+++ frontend/src/components/admin/ButtonMailSend.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import MessageMailDelete from "./MessageMailDelete";
 import "./styles/ButtonMailSend.css";
 
 function ButtonMailSend() {
+  const [isAdminClick, setIsAdminClick] = useState(false);
+  function handleClick(e) {
+    e.preventDefault();
+    setIsAdminClick(!isAdminClick);
+
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/sendsorrymail`, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(),
+    }).then((response) => {
+      if (response.status === 200);
+      return response.json();
+    });
+  }
   return (
     <div className="containbuttonmailsend">
-      <button type="submit" className="buttonmailsend">
+      <button
+        type="submit"
+        className="buttonmailsend"
+        value="submit"
+        onClick={handleClick}
+      >
         <h3>Envoyer</h3>
       </button>
+      {isAdminClick && <MessageMailDelete />}
     </div>
   );
 }
--- frontend/src/components/post/CardPost/PostCard.jsx
+++ frontend/src/components/post/CardPost/PostCard.jsx
@@ -1,51 +1,21 @@
-/* import React from "react";
-import "./styles/Postcard.css";
-import { propTypes } from "react-bootstrap/esm/Image";
-import ButtonLikeDislike from "../ButtonLikeDislike";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./styles/PostList.css";
 
-function PostCard({ post }) {
-  const { user, content, category, postdate } = post;
-  // const backgroundColorList = [
-  //   "#303364",
-  //   "#EC4D4D",
-  //   "#A7D1CD",
-  //   "#F8C053",
-  //   "#A098C9",
-  // ];
-  // const rand = Math.floor(Math.random() * backgroundColorList.length);
-  // const valueColor = backgroundColorList[rand];
+export default function PostList() {
+  const [/* posts, */ setPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}/post`)
+      .then((res) => setPosts(res.data))
+      .catch((err) => {
+        console.warn(err.response.data.error);
+      });
+  }, []);
   return (
-    <div className="postcard">
-      <div className="headercard">
-        <h2>
-          <span className="dateText"> {postdate} 2020-02-12 </span> {user}
-          user1100{" "}
-        </h2>
-        <p> {category} porno </p>
-      </div>
-      <div className="contentcard">
-        <p>{content}</p>
-      </div>
-      <div className="footercard">
-        <ButtonLikeDislike />
-      </div>
+    <div className="postList">
+      <div className="post" />
     </div>
   );
 }
-PostCard.propTypes = {
-  post: propTypes.string,
-  user: propTypes.string,
-  content: propTypes.string,
-  category: propTypes.string,
-  postdate: propTypes.instanceOf(Date),
-};
-
-PostCard.defaultProps = {
-  post: propTypes.string,
-  user: propTypes.string,
-  content: propTypes.string,
-  category: propTypes.string,
-  postdate: propTypes.instanceOf(Date),
-};
-
-export default PostCard; */
